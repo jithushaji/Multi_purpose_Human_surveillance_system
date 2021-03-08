fbbb698f7aa0cd000ae1a9a08f81d7a9468e3b03
jithu <jithushaji23@gmail.com>
2021-03-08 18:55:50 +0530

    Initial commit

---
+++ Capture_Image.py
@@ -0,0 +1,80 @@
+import csv
+import sqlite3
+import cv2
+import os
+import imutils
+
+createtable="""CREATE TABLE IF NOT EXISTS Identity (ID INTEGER PRIMARY KEY,Name TEXT NOT NULL,age INTEGER,gender TEXT,remark TEXT);"""
+connection=sqlite3.connect("survilance.db")
+cursor=connection.cursor()
+cursor.execute(createtable)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+
+
+# Take image function
+
+def takeImages():
+
+
+    Id = input("Enter Id: ")
+    name = input("Enter Name: ")
+    age=input("Enter age:")
+    gender=input("Enter gender:")
+    remark=input("Enter remark:")
+
+    if(is_number(Id) and remark.isalpha()):
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+
+        while(True):
+            ret, img = cam.read()
+            img=imutils.resize(img, width=400)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5, minSize=(30,30),flags = cv2.CASCADE_SCALE_IMAGE)
+            for(x,y,w,h) in faces:
+                cv2.rectangle(gray, (x, y), (x+w, y+h), (10, 159, 255), 1)
+                #incrementing sample number
+                sampleNum = sampleNum+1
+                #saving the captured face in the dataset folder TrainingImage
+                cv2.imwrite("TrainingImage" + os.sep +name + "."+Id + '.' +
+                            str(sampleNum) + ".jpg", gray[y:y+h, x:x+w])
+                #display the frame
+                cv2.imshow('frame', gray)
+            #wait for 100 miliseconds
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            # break if the sample number is more than 100
+            elif sampleNum > 100:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        res = "Images Saved for ID : " + Id + " Name : " + name
+        row = (Id,name,age,gender,remark)
+        saverow="INSERT INTO Identity (ID,Name,age,gender,remark) VALUES (?, ?, ?, ?, ?)"
+        cursor.execute(saverow,row)
+        connection.commit()
+        connection.close()
+    else:
+        print("Check Entered Data")
+
+
+
---
+++ Recognize.py
@@ -0,0 +1,88 @@
+import datetime
+import os
+import time
+import imutils
+import cv2
+import pandas as pd
+import sqlite3
+
+#-------------------------
+def recognize_face():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("TrainingImageLabel"+os.sep+"Trainner.yml")
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath)
+    connection=sqlite3.connect("survilance.db")
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    query="CREATE TABLE IF NOT EXISTS Recognized (ID INTEGER PRIMARY KEY ,Name TEXT NOT NULL,Age INTEGER,Gender TEXT,Remark TEXT,Time Text);"
+    cursor=connection.cursor()
+    cursor.execute(query)
+    query2="SELECT * FROM Identity WHERE ID=?"
+    query3="INSERT OR IGNORE INTO Recognized (ID,Name,Age,Gender,Remark,Time) VALUES (? ,?, ?, ?, ?, ?)"
+
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    # Define min window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
+    while True:
+        ret, im = cam.read()
+        im=imutils.resize(im, width=400)
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(im, 1.2, 5,minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x+w, y+h), (10, 159, 255), 1)
+            Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
+            cursor.execute(query2,(Id,))
+            records=cursor.fetchall()
+
+            if conf < 100:
+
+                for row in records:
+                    aa = row[1]
+                confstr = "  {0}%".format(round(100 - conf))
+                tt = str(Id)+"-"+aa
+
+
+            else:
+                Id = '  Unknown  '
+                tt = str(Id)
+                confstr = "  {0}%".format(round(100 - conf))
+
+            if (100-conf) > 40:
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                for dat in records:
+                    Name=dat[1]
+                    Age=dat[2]
+                    Gender=dat[3]
+                    Remark=dat[4]
+                val=(Id,Name,Age,Gender,Remark,timeStamp)
+                cursor.execute(query3,val)
+                connection.commit()
+
+            tt = str(tt)[2:-2]
+
+
+            if(100-conf) > 30:
+                cv2.putText(im, str(tt), (x+5,y-5), font, 1, (255, 255, 255), 2)
+            else:
+                cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+
+            if (100-conf) > 27:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
+            elif (100-conf) > 20:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 255), 1)
+            else:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 0, 255), 1)
+
+
+        cv2.imshow('Recognizer', im)
+        if (cv2.waitKey(1) == ord('q')):
+            break
+    connection.close()
+    print("Success")
+    cam.release()
+    cv2.destroyAllWindows()
